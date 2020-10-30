d02ccac9516000956ed53fa276c7b9f38f7ae38a
Kelvin Sudani <sudanikelvin@gmail.com>
2020-10-30 17:04:20 +0530

    delivery 0: showing the weather data on console or catching errors

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const KEY = process.env.KEY
 const locationFromArgs = process.argv.slice(2)
 
 const apiCall = async (item) => {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${item}&appid=${KEY}`
+  let url = ''
+  const checkIfInteger = parseInt(item) //input arg can be zipcode as well
+
+  if (checkIfInteger) {
+    url = `https://api.openweathermap.org/data/2.5/weather?zip=${item}&appid=${KEY}`
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${item}&appid=${KEY}`
+  }
   try {
     const response = await axios.get(url)
     const data = response.data
     console.log(`---> Weather Data for "${item}" : `)
     console.log(data)
   } catch (error) {
-    console.log(error)
+    const errorResponse = `---> Error with location : "${item}"====> ${error.response.data.cod} ${error.response.data.message}`
+    console.log(errorResponse)
   }
 }
 
-apiCall('Boston')
+//check for empty arg list
+locationFromArgs.length > 0
+  ? locationFromArgs.forEach(apiCall)
+  : console.log(
+      'Please add some arguments ... example: node index.js Boston Dallas London'
+    )
