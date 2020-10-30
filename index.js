af7cab0a4242dc864345326007d3bb1a357c2826
Kelvin Sudani <sudanikelvin@gmail.com>
2020-10-30 16:54:05 +0530

    test api: testing api wby passing a location
    Checking the validity of the key and respons from the api sever.

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ const axios = require('axios')
 
 require('dotenv').config()
 const KEY = process.env.KEY
-const args = process.argv.slice(2)
-console.log('Command Line arguments are: ', args)
+const locationFromArgs = process.argv.slice(2)
+
+const apiCall = async (item) => {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${item}&appid=${KEY}`
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+    console.log(`---> Weather Data for "${item}" : `)
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+apiCall('Boston')
